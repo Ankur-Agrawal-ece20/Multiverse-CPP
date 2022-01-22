9e6da84972f2e295c39cb7b5a740575c94577aac
Ankur Agrawal <ankuragrawal9455@gmail.com>
2022-01-22 15:45:18 +0530

    Udating

--- file.cpp
+++ file.cpp
@@ -1,14 +1,8 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
-
-TEST_CASE("testing the factorial function") {
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+int factorial(int number){ 
+    return number <= 1 ? number : factorial(number - 1) * number; 
 }
+
+#include "unit-test/testcase.h"
