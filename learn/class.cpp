f1bcd00ac372bf721241393dd9b641f3adc3751a
Ankur Agrawal <78701055+Ankur-Agrawal-ece20@users.noreply.github.com>
2022-02-07 13:13:51 +0000

    Added replit features

---
+++ learn/class.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    cout<<(2*n);
+}
+
+int main() {
+
+    int testcase=1;
+    cin>>testcase;
+    for(int i=0;i<testcase;i++){
+        // cout<<"Case #"<<i<<": ";
+        solve();
+        cout<<"\n";
+    }
+    
+    return 0;
+}
