5da39a616e85dcfceed030292d67c43c37c1bb3e
Ankur Agrawal <78701055+Ankur-Agrawal-ece20@users.noreply.github.com>
2022-02-07 13:58:57 +0000

    Adding code

--- learn/class.cpp
+++ learn/class.cpp
@@ -1,20 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    int n; cin>>n;
-    cout<<(2*n);
+class Mall{
+  protected:
+  int basket_id;
+};
+
+class Basket: public Mall
+{
+  private:
+  int n=5;
+
+  public:
+  // Basket(){
+  //   cout<<"Constructor is called\n";
+  // }
+  Basket(int x){
+    n=x;
+  }
+  ~Basket(){
+    cout<<"Basket with "<<n<<" items is destroyed\n";
+  }
+  void set_id(int i){
+    basket_id=i;
+  }
+  void print_id(){
+    cout<<"Basket_id is "<<basket_id<<"\n";
+  }
+  void count();
+  // void count(){
+  //   if(n==0){
+  //     cout<<"BASKET IS EMPTY\n";
+  //   }
+  //   else{
+  //     cout<<n<<" items in basket\n";
+  //   }
+  // }
+};
+
+void Basket::count(){
+  if(n==0) cout<<"Basket is empty\n";
+  else cout<<n<<" items in basket\n";
 }
 
 int main() {
 
-    int testcase=1;
-    cin>>testcase;
-    for(int i=0;i<testcase;i++){
-        // cout<<"Case #"<<i<<": ";
-        solve();
-        cout<<"\n";
+    Basket basket1(10);
+    basket1.count();
+    basket1.set_id(25);
+    basket1.print_id();
+
+    for(int i=1;i<=5;i++){
+      Basket basket(i);
+      basket.count();
     }
-    
+    // cout<<basket1.m<<"\n";
     return 0;
 }
