325bec2fb92669e8138d80811dddfbc73b8ef24b
Abhinav Jha <linuxlenin04@gmail.com>
2022-02-17 13:15:27 +0530

    Task 2 completed!

--- src/Task2/task2.cpp
+++ src/Task2/task2.cpp
@@ -19,13 +19,13 @@ class Solution {
             std::vector<std::vector<unsigned int>> container;
 
             // Group the vector's elements into groups of two
-            for (int i{}; i < std::size(v_temp); i += 2) {
+            for (int i{}; i < std::size(v_temp); i += 2) {            // std::size needs C++17
                 std::vector<unsigned int> temp = {v_temp[i], v_temp[i+1]};
                 container.push_back(temp);
             }
 
             // Store the second elements in an array of their own, sort them, then use a map for referencing group
-            std::map<unsigned int, std::vector<unsigned int>&> el_map;
+            std::map<unsigned int, std::vector<unsigned int>> el_map;
             std::vector<unsigned int> seconds;
 
             // Mapping the second elements to the ref of their groups
@@ -42,7 +42,32 @@ class Solution {
         }
         void solve_task3() {
             // solve task3 and save the answer in ans3
+            std::vector<unsigned int> v_temp = v;
+            std::vector<std::vector<unsigned int>> container;
+            std::queue<unsigned int> q;
 
+            // Group the vector's elements into groups of two
+            for (int i{}; i < std::size(v_temp); i += 2) {            // std::size needs C++17
+                std::vector<unsigned int> temp = {v_temp[i], v_temp[i+1]};
+                container.push_back(temp);
+            }
+
+            // Loop through the first elements of the groups
+            for (const auto& val : container) {
+                if (val[0] % 2 == 0) {
+                    q.push(val[1]);
+                } 
+                else {
+                    q.push(val[1]);
+                    q.pop();                        
+                }
+            }
+
+            // Now loop through the queue and assign the values to ans3
+            for (int i = 0; i < std::size(q); ++i) {
+                this->ans3.push_back(q.front());
+                q.pop();
+            }
         }
     
     public:
@@ -50,33 +75,66 @@ class Solution {
         Solution(int n, std::vector<unsigned int> v) {
             this->n = n;
             this->v = v;
+            this->solve_task1();
+            this->solve_task2();
+            this->solve_task3();
         }
 
     string FINDMATCH(string path) {
         // complete this function to read file, compare with ans1, ans2, ans3
         // and return the answer
+        std::vector<unsigned int> v;
+
         fstream file;
 
         file.open(path, std::ios::in | std::ios::binary);
+
+        file.seekg(0, std::ios::end);
+        int length = file.tellg();
+
+        file.seekg(0, std::ios::beg);
+        while (file.tellg() != length) {
+            int x;
+            file.read((char*)(&x), sizeof(int));
+            v.push_back(x);
+        }
+
+        if (v == this->ans1) {
+            return "TASK1";
+        }
+        else if (v == this->ans2) {
+            return "TASK2";
+        }
+        else if (v == this->ans3) {
+            return "TASK3";
+        }
+        else {
+            return "NOTFOUND";
+        }
+
     }
 };
 
 
 
-// int main() {
-//     vector<unsigned int> a{5, 7, 6, 5, 2, 1, 4, 0, 1, 3};
-//     string paths[] = {
-//         "missing_files/missing1.bin",
-//         "missing_files/missing2.bin",
-//         "missing_files/missing3.bin",
-//         "missing_files/missing4.bin",
-//     };
-//     // Intialize your solution object here
+int main() {
+    vector<unsigned int> a{5, 7, 6, 5, 2, 1, 4, 0, 1, 3};
+    string paths[] = {
+        "missing_files/missing1.bin",
+        "missing_files/missing2.bin",
+        "missing_files/missing3.bin",
+        "missing_files/missing4.bin",
+    };
+    // Intialize your solution object here
+    Solution sol(10, a);
 
-//     // Make a for loop to go through paths array and call FINDMATCH function
+    // Make a for loop to go through paths array and call FINDMATCH function
+    for (auto& s : paths) {
+        sol.FINDMATCH(s);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 // ==> NOTE: Comment main function and uncomment below line to verify your code 
-#include "../../include/test2_cases.h"
+// #include "../../include/test2_cases.h"
