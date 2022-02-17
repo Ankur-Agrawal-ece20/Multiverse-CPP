9a3165806bb682ff8b27bdddefdcfe27aaf44772
Adrito-M <adrito.mukherjee.cse21@itbhu.ac.in>
2022-02-17 18:07:26 +0530

    task1 completed, passes all tests

--- src/Task1/task1.cpp
+++ src/Task1/task1.cpp
@@ -1,26 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(int n, vector<string> arr){
-    int answer=0;
-    // Start your code here
+vector<string> seperate(string& args) {
+    
+    vector<string> v;
+    string delim = ", ";
+    size_t start = 0U;
+    size_t end = args.find(delim);
+    while (end != std::string::npos)
+    {
+        v.push_back(args.substr(start, end - start));
+        start = end + delim.length();
+        end = args.find(delim, start);
+    }
 
-    // End your code here
-    return answer;
+    v.push_back(args.substr(start, end));
+    return v;
 }
 
-int main(){
-    // int n;
-    // cin>>n;
-    // cin.ignore();
-    // vector<string> arr(n);
-    // for(int i=0;i<n;i++){
-    //     getline(cin,arr[i]);
-    //     cout<<arr[i]<<endl;
-    // }
-    // cout<<solve(n,arr);
-    return 0;
+
+void do_add(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] + variables[params[2]];
 }
 
-// ==> NOTE: Comment main function and uncomment below line to verify your code 
-// #include "../../include/test1_cases.h"
+
+void do_sub(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] - variables[params[2]];
+}
+
+
+void do_addi(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] + stoi(params[2], nullptr);
+}
+
+
+void do_mul(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] * variables[params[2]];
+}
+
+
+void do_and(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] & variables[params[2]];
+}
+
+
+void do_or(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] | variables[params[2]];
+}
+
+
+void do_andi(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] & stoi(params[2], nullptr);
+}
+
+
+void do_ori(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = variables[params[1]] | stoi(params[2], nullptr);
+}
+
+
+void do_li(vector<string>& params, unordered_map<string, int>& variables) {
+    variables[params[0]] = stoi(params[1], nullptr);
+}
+
+int solve(int n, vector<string>& arr){
+
+    unordered_map<string, int> variables;
+
+    for (string& command : arr) {
+
+        string instruction = command.substr(0, command.find(' '));
+        string rest = command.substr(command.find(' ') + 1);
+        vector<string> params = seperate(rest);
+        
+        if (instruction == "add") do_add(params, variables);
+        else if (instruction == "sub") do_sub(params, variables);
+        else if (instruction == "addi") do_addi(params, variables);
+        else if (instruction == "mul") do_mul(params, variables);
+        else if (instruction == "and") do_and(params, variables);
+        else if (instruction == "or") do_or(params, variables);
+        else if (instruction == "andi") do_andi(params, variables);
+        else if (instruction == "ori") do_ori(params, variables);
+        else if (instruction == "li") do_li(params, variables);
+        else if (instruction == "sw") {
+            return variables[params[0]];
+        }
+    }
+    return -1;
+}
+
+
+
+
+
+
+#include "../../include/test1_cases.h"
