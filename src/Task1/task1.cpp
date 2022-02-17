55dcf30bf90ce127af3aadaeedd4ccb8ff3c5583
amyra98 <priti2002bwn@gmail.com>
2022-02-17 17:28:39 +0530

    Task done

--- src/Task1/task1.cpp
+++ src/Task1/task1.cpp
@@ -1,26 +1,128 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+map<string,int>val;
+
+void for_simplifying(string operation,string 1,string 2,string 3)
+{
+    if(operation=="add")
+    {
+        val[1]=val[2]+val[3];
+
+    }
+    else if(operation=="li")
+    {
+    stringstream string_to_num(2);
+
+
+        int num=0;
+	string_to_num>>num;
+
+        val[1]=num;
+
+    }
+    else if(operation=="sub")
+    {
+        val[1]=val[2]-val[3];
+    }
+    else if(operation=="mul")
+    {
+        val[1]=val[2]*val[3];
+    }
+    else if(operation=="addi")
+    {
+    stringstream string_to_num(3);
+
+
+int num=0;
+	string_to_num>>num;
+
+
+
+     val[1]=val[2]+num;
+    }
+     else if(operation=="and")
+    {
+         val[1]=val[2]&val[3];
+    }
+     else if(operation=="or")
+    {
+          val[1]=val[2]|val[3];
+    }
+     else if(operation=="andi")
+    {
+    stringstream string_to_num(3);
+	int num=0;
+	string_to_num>>num;
+
+
+     val[1]=val[2]&num;
+    }
+     else if(operation=="ori")
+     {
+    stringstream string_to_num(3);
+	int num=0;
+	string_to_num>>num;
+
+
+     val[1]=val[2]|num;
+     }
+    else if(operation=="sw")
+    {
+        val["answer"]=val[1];
+    }
+}
+
 int solve(int n, vector<string> arr){
     int answer=0;
     // Start your code here
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        for (int j = 0; j < arr[i].size(); j++)
+        {
+            if(arr[i][j] !=',')
+            {
 
+
+s+=arr[i][j];
+            }
+        }
+        arr[i]=s;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        stringstream ss;
+        string str=arr[i];
+        ss<<str;
+        ss>>str;
+        string operation=str;
+        ss>>str;
+        string 1=str;
+        ss>>str;
+        string 2=str;
+        ss>>str;
+        string 3=str;
+        for_simplifying(operation,1,2,3);
+    }
+    answer = val["answer"];
     // End your code here
     return answer;
 }
 
-int main(){
-    // int n;
-    // cin>>n;
-    // cin.ignore();
-    // vector<string> arr(n);
-    // for(int i=0;i<n;i++){
-    //     getline(cin,arr[i]);
-    //     cout<<arr[i]<<endl;
-    // }
-    // cout<<solve(n,arr);
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     cin.ignore();
+//     vector<string> arr(n);
+//     for(int i=0;i<n;i++){
+//         getline(cin,arr[i]);
+//         cout<<arr[i]<<endl;
+//     }
+//     cout<<solve(n,arr);
+//     return 0;
+// }
 
-// ==> NOTE: Comment main function and uncomment below line to verify your code 
-// #include "../../include/test1_cases.h"
+// ==> NOTE: Comment main function and uncomment below line to verif2 2our code
+#include "../../include/test1_cases.h"
--- src/Task2/task2.cpp
+++ src/Task2/task2.cpp
@@ -1,46 +1,155 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define pb push_back
 
-
-class Solution {
-    private:
-    int n; // n is size that user inputs
-    vector<unsigned int>v,ans1,ans2,ans3; // v is the vector that user inputs
-    void solve_task1(){
+class Solution
+{
+private:
+    int n;                                    // n is size that user inputs
+    vector<unsigned int> v, ans1, ans2, ans3; // v is the vector that user inputs
+    void solve_task1()
+    {
         // solve task1 and save the answer in ans1
+        set<int> unique;
+        for (int i = 0; i < n; i++)
+        {
+            unique.insert(v[i]);
+        }
+        set<int>::iterator sort;
+        for (sort = s.begin(); sort != s.end(); sort++)
+        {
+            ans1.pb(*sort);
+        }
     }
-    void solve_task2(){
+    bool sortbyfirst(const pair<int,int> &a,const pair<int,int> &b)
+{
+    if (a.second==b.second)
+    {
+        return(a.first<b.first);
+    }
+    else
+    return (a.second > b.second);
+}
+    void solve_task2()
+    {
         // solve task2 and save the answer in ans2
+        vector<pair<int,int>>req;
+        for (int i = 0; i < n-1; i=i+2)
+        {
+            int one=v[i];
+            int two=v[i+1];
+            req.pb({one,two});
+        
+        sort(req.begin(), req.end(), sortbyfirst);
+        for (int i = 0; i <req.size() ; i++)
+        {
+            ans2.pb(req[i].first);
+        }
     }
-    void solve_task3(){
+    void solve_task3()
+    {
         // solve task3 and save the answer in ans3
-    }
-    
-    public:
-    // create a constructor to take input
+        vector<pair<int,int>>t;
+        for (int i = 0; i < n-1; i=i+2)
+        {
+            int one=v[i];
+            int two=v[i+1];
+            t.pb({one,two});
+        }
+        queue<int>que;
+        for (int i = 0; i < req.size(); i++)
+        {
+            if(req[i].first%2 !=0)
+            {
+                que.push(req[i].second);
+            }
+            else
+            {
+                que.push(req[i].second);
+                que.pop();
+            }
+        }
 
-    string FINDMATCH(string path){
+        while(!que.empty())
+        {
+            int one=que.front();
+            ans3.pb(one);
+            que.pop();
+        }        
+    }
+
+public:
+    // create a constructor to take input
+    void solution(int size,vector<int> a)
+    {
+        n=size;
+        a = v;
+        solve_task1();
+        solve_task2();
+        solve_task3();
+    }
+
+    string FINDMATCH(string path)
+    {
         // complete this function to read file, compare with ans1, ans2, ans3
         // and return the answer
+        ifstream binaryfile;
+        binaryfile.open(path,ios::binary);
+
+         if (!binaryfile.is_open()){
+            cout<<"The file can't be opened.\n";
+            return "ERROR";
+        }
+        binaryfile.seekg(0, ios::end);
+        int length = binaryfile.tellg();
+        binaryfile.seekg(0, ios::beg);
+
+        vector<int> answer;
+
+        while(binaryfile.tellg() != length)
+        {
+            int x;
+            binaryfile.read((char *)&x,sizeof(int));
+            answer.pb(x);
+        }
+
+        if (ans1==answer) 
+        {
+            return "TASK1";
+        }
+        else if (ans2==answer) 
+        {
+            return "TASK2";
+        }
+        else if (ans3==answer) 
+        {
+            return "TASK3";
+        }
+        else
+        {
+             return "NOTFOUND";
+        }
     }
 };
 
+// int main()
+// {
+//     vector<unsigned int> a{5, 7, 6, 5, 2, 1, 4, 0, 1, 3};
+//     string paths[] = {
+//         "missing_files/missing1.bin",
+//         "missing_files/missing2.bin",
+//         "missing_files/missing3.bin",
+//         "missing_files/missing4.bin",
+//     };
+//     // Intialize your solution object here
+//     solution ob(a.size(),a);
+//     // Make a for loop to go through paths array and call FINDMATCH function
+//       for(string path : paths){
+//         cout<<ob.FINDMATCH(path)<<endl;
+//     }
 
+//     return 0;
+// }
 
-int main(){
-    vector<unsigned int>a{5,7,6,5,2,1,4,0,1,3};
-    string paths[]={
-        "missing_files/missing1.bin",
-        "missing_files/missing2.bin",
-        "missing_files/missing3.bin",
-        "missing_files/missing4.bin",
-    };
-    // Intialize your solution object here
-
-    // Make a for loop to go through paths array and call FINDMATCH function
-
-    return 0;
-}
-
-// ==> NOTE: Comment main function and uncomment below line to verify your code 
-// #include "../../include/test2_cases.h"
+// ==> NOTE: Comment main function and uncomment below line to verify your code
+#include "../../include/test2_cases.h"
